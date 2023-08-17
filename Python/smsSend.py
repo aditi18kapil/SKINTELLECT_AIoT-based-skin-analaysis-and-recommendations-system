d6057eccb9f2f703036a8d0171f1e0a5c72c9643
aditi18kapil <118043393+aditi18kapil@users.noreply.github.com>
2023-08-17 09:47:15 +0530

    Update smsSend.py

--- Python/smsSend.py
+++ Python/smsSend.py
@@ -4,11 +4,11 @@
 def send_sms(ph_no,result_text,recommendation):
     from twilio.rest import Client
     final_text = result_text+"\n"+"\n"+recommendation
-    SID = 'ACa0c77eaa8f7d080f5c8bb826c887ba98'
-    AUTH_TOKEN = 'd0effba4995c97f648caa1bc97e316a0'
+    SID = 'AC30dc3d3a14755fdd175edf525321a404'
+    AUTH_TOKEN = '304388f7d60e58434d0e594d2a51340e'
 
     cl = Client(SID, AUTH_TOKEN)
 
-    cl.messages.create(body=final_text, from_='+13252465808', to=ph_no)
+    cl.messages.create(body=final_text, from_='+18645275279', to=ph_no)
 
     print("SMS SENT SUCCESSFULLY ! ")
